34c665cd7e62542a9d34ff2ca3d75730dd31f79b
Oleg <vasyanin-o-v-166030@mail.ru>
2020-11-18 00:02:06 +0300

    another files were added

--- Problems/Comparing strings ignoring whitespaces/src/Main.java
+++ Problems/Comparing strings ignoring whitespaces/src/Main.java
@@ -4,8 +4,8 @@ class Main {
     public static void main(String[] args) {
         // put your code here
         Scanner scanner = new Scanner(System.in);
-        String first = scanner.nextLine().replace(" ","");
-        String second = scanner.nextLine().replace(" ","");
+        String first = scanner.nextLine().replace(" ", "");
+        String second = scanner.nextLine().replace(" ", "");
         System.out.println(first.equals(second));
     }
 }
